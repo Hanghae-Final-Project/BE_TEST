5dec696286c733591512abaf64a7c79f1830873b
lee58012 <gian58012@gmail.com>
2022-06-30 21:49:02 +0900

    first

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const User = require('../schemas/usersSchema');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    res.status(401).send({
+      errorMessage: '로그인 후 사용하세요',
+    });
+    return;
+  }
+
+  const [tokenType, tokenValue] = authorization.split(' ');
+
+  if (tokenType !== 'Bearer') {
+    res.status(401).send({
+      errorMessage: '로그인 후 사용하세요',
+    });
+    return;
+  }
+
+  try {
+    const { email } = jwt.verify(tokenValue, process.env.JWT_SECRET_KEY);
+
+    User.findOne({ email })
+      .exec()
+      .then((user) => {
+        res.locals.user = user;
+        next();
+      });
+  } catch (error) {
+    res.status(401).send({
+      errorMessage: '로그인 후 사용하세요',
+    });
+    return;
+  }
+};
