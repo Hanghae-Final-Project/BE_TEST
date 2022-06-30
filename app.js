3343ee660d85d4ff306360b076c40de156ad21d7
pieceofcakey <choi_927@naver.com>
2022-06-30 17:14:34 +0900

    코멘트 및 마이페이지 기능 추가

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config;
+const connect = require('./schemas');
+
+app.use(cors({ origin: true, credentials: true }));
+
+connect();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api/comments', require('./routes/commentsRouter.js'));
+app.use('/api', require('./routes/usersRouter.js'));
+
+app.use((req, res, next) => {
+  console.log('Request URL:', req.originalUrl, ' - ', new Date());
+  next();
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on 3000`);
+});
